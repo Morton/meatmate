3c7cb728e36129004df88830ce9eef8c6d9f5b30
Martin Heller <github@technopolis.de>
2025-01-12 20:38:35 +0100

    Add logo to app

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,14 @@ import { Toolbar, Typography, Box } from "@mui/material";
 import TemperaturePanel from "./components/TemperaturePanel";
 import BluetoothButton from "./components/BluetoothButton";
 import TemperatureHistoryChart from "./components/TemperatureHistoryChart";
+import logo from "./logo.webp";
 
 function App() {
   return (
     <>
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1, color: "#ffab40" }}>
+        <img src={logo} alt="" style={{ width: 40, height: 40 }} />
+        <Typography variant="h6" sx={{ flexGrow: 1, color: "#ffab40", marginLeft: 1 }}>
           Meat mate
         </Typography>
         <BluetoothButton />
